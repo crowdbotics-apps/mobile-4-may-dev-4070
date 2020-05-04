2338129a87b13049e2e869662735043fe3394e23
Crowdbotics <team@crowdbotics.com>
2020-05-04 07:44:53 +0000

    add package blueprint-react-native-emailauth-screen

--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import EmailAuth3614Reducer from '../features/EmailAuth3614/redux/reducers';
 import EmailAuth3612Reducer from '../features/EmailAuth3612/redux/reducers';
 import EmailAuthReducer from '../features/EmailAuth/redux/reducers';
 
@@ -17,6 +18,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+EmailAuth3614: EmailAuth3614Reducer,
 EmailAuth3612: EmailAuth3612Reducer,
 EmailAuth: EmailAuthReducer,
 
